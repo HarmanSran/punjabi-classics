e50f6b48a2fc1e633137a1be1832602c8f810f9f
Harman Sran <iharmandeep@gmail.com>
2021-05-24 17:46:53 -0400

    basic appbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import AppToolbar from './components/AppToolbar/AppToolbar';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#192978',
+    },
+  },
+});
 
 const App = ({ lyrics }) => (
-  <div>
+  <ThemeProvider theme={theme}>
+    <AppToolbar />
     <ul>
       {lyrics.map((lyric) => <li>{lyric.link}</li>)}
+      {lyrics.map((lyric) => <li>{lyric.link}</li>)}
+      {lyrics.map((lyric) => <li>{lyric.link}</li>)}
+      {lyrics.map((lyric) => <li>{lyric.link}</li>)}
+      {lyrics.map((lyric) => <li>{lyric.link}</li>)}
+      {lyrics.map((lyric) => <li>{lyric.link}</li>)}
+      {lyrics.map((lyric) => <li>{lyric.link}</li>)}
+      {lyrics.map((lyric) => <li>{lyric.link}</li>)}
+      {lyrics.map((lyric) => <li>{lyric.link}</li>)}
+      {lyrics.map((lyric) => <li>{lyric.link}</li>)}
     </ul>
-  </div>
+  </ThemeProvider>
 );
 
 App.propTypes = {
