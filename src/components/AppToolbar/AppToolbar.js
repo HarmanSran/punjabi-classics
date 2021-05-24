01db26bb9a5219875276ec7259a4f0621b9b9a90
Harman Sran <iharmandeep@gmail.com>
2021-05-24 18:06:10 -0400

    refactor appbar

--- src/components/AppToolbar/AppToolbar.js
+++ src/components/AppToolbar/AppToolbar.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles } from '@material-ui/core/styles';
+import SearchField from './components/SearchField';
+import GithubButton from './components/GithubButton';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
@@ -22,45 +17,14 @@ const useStyles = makeStyles((theme) => ({
       display: 'block',
     },
   },
-  rightHandSide: {
+  actions: {
+    display: 'flex',
     position: 'relative',
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(1),
       width: 'auto',
-    },
-  },
-  search: {
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
     },
   },
 }));
@@ -72,31 +36,12 @@ const AppToolbar = () => {
     <div className={classes.root}>
       <AppBar position="fixed" color="primary">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
             Punjabi Classics
           </Typography>
-          <div className={classes.rightHandSide}>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-              />
-            </div>
+          <div className={classes.actions}>
+            <SearchField />
+            <GithubButton />
           </div>
         </Toolbar>
       </AppBar>
---
+++ src/components/AppToolbar/components/GithubButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import { repository } from '../../../../package.json';
+
+const GithubButton = () => (
+  <Tooltip title="Source" aria-label="source">
+    <IconButton
+      edge="start"
+      color="inherit"
+      aria-label="open github"
+      size="small"
+      onClick={() => window.open(repository)}
+    >
+      <GitHubIcon />
+    </IconButton>
+  </Tooltip>
+);
+
+export default GithubButton;
